0c4d9a8faa44e0f4823dad669fb62397a96e6096
Renan Schotten <renan.schotten@gmail.com>
2022-08-23 15:33:06 -0300

    reusing instantiation

--- src/model/entities/Department.java
+++ src/model/entities/Department.java
@@ -1,11 +1,12 @@
 package model.entities;
 
 import java.io.Serializable;
+import java.sql.ResultSet;
 
 public class Department implements Serializable {
-	
+
 	private static final long serialVersionUID = 1L;
-	
+
 	private Integer id;
 	private String name;
 
